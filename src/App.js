79e2675926fbd7c5bd9e0e5e8131794fbcf92223
Amel <amcenp@gmail.com>
2018-09-05 03:29:18 +0200

    Some changes made

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import Persons from './components/persons';
+import Persons from './components/Persons';
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <div className="row nebo">
-           <div className="col-md-3">
+      <div className="App">
+        <div className="nebo container">
+          <div className="row">
+            <div className="col-md-3">
               <div className="form-group inputWrapper">
-                  <h5 className="textLabel">Please enter your data</h5>
-                   <input type="text" className="form-control inputFilds" id="name" placeholder="Name"/>
-                   <input type="text" className="form-control inputFilds" id="name" placeholder="Surname"/>
-                   <input type="text" className="form-control inputFilds" id="name" placeholder="City"/>
-                   <input type="text" className="form-control inputFilds" id="name" placeholder="Address"/>
-                   <input type="text" className="form-control inputFilds" id="name" placeholder="Phone"/>
-                   
+                <h5 className="textLabel">Please enter your data</h5>
+                <input type="text" className="form-control inputFilds" id="name" placeholder="Name" />
+                <input type="text" className="form-control inputFilds" id="name" placeholder="Surname" />
+                <input type="text" className="form-control inputFilds" id="name" placeholder="City" />
+                <input type="text" className="form-control inputFilds" id="name" placeholder="Address" />
+                <input type="text" className="form-control inputFilds" id="name" placeholder="Phone" />
+
               </div>
-           </div>
-           <Persons />
-              
+            </div>
+            <Persons />
+
+          </div>
         </div>
       </div>
     );
--- src/components/persons.js
+++ src/components/persons.js
@@ -7,64 +7,65 @@ class Person extends Component {
         name: '',
         surname: '',
         city: '',
-        address:'',
+        address: '',
         phone: '',
     }
 
 
-    componentDidMount () {
+    componentDidMount() {
         axios.get('https://phonebookapi.herokuapp.com/api/allPersons')
-        .then(response =>  {
-            this.setState ({...this.state, persons: response.data.persons});
-        });
+            .then(response => {
+                this.setState({ ...this.state, persons: response.data.persons });
+            });
     }
 
-    render () {
+    render() {
         console.log(this.state);
         if (!this.state.persons) {
             return <h1>...Loading</h1>
-        }else {
+        } else {
             return (
                 <div className="col-md-9 tablePost">
-                      <table className="table">
-                          <thead>
+                    <table className="table">
+                        <thead>
                             <tr>
-                              <th scope="col">Name</th>
-                              <th scope="col">Surname</th>
-                              <th scope="col">Created Data</th>
-                              <th scope="col">City</th>
-                              <th scope="col">Address</th>
-                              <th scope="col">Phone</th>
-                              <th scope="col">Actions</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Surname</th>
+                                <th scope="col">Created Data</th>
+                                <th scope="col">City</th>
+                                <th scope="col">Address</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Actions</th>
                             </tr>
-                          </thead>
-                          <tbody>
+                        </thead>
+                        <tbody>
                             {this.state.persons.map(person => {
-                                
+
                                 let godina = person.createdAt.slice(0, 4);
                                 let mesec = person.createdAt.slice(5, 7);
                                 let dan = person.createdAt.slice(8, 10);
                                 let fullDate = dan + '/' + mesec + '/' + godina;
-                                
 
-                                return(
+
+                                return (
                                     <tr>
-                            <td>{person.name}</td>
-                            <td>{person.surname}</td>
-                            <td>{fullDate}</td>
-                            <td>{person.city}</td>
-                            <td>{person.address}</td>
-                            <td>{person.phone}</td>
-                            <td>@mdo</td>
-                          </tr>
+                                        <td>{person.name}</td>
+                                        <td>{person.surname}</td>
+                                        <td>{fullDate}</td>
+                                        <td>{person.city}</td>
+                                        <td>{person.address}</td>
+                                        <td>{person.phone}</td>
+                                        <td>@mdo</td>
+                                    </tr>
                                 );
                             })}
-                           
-                          </tbody>
-                        </table>
-                  </div>
-            ) 
-        }     }
+
+                        </tbody>
+                    </table>
+                </div>
+            )
+        }
+    }
 }
 
 
