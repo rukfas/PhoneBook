989d054239198a6634070c15ae2b9903b420394d
dopaminos <farukbruncevic@rocketmail.com>
2018-09-07 03:43:00 +0200

    edit button open modal and editing users data

--- src/App.js
+++ src/App.js
@@ -1,56 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
 import Persons from './components/Persons';
-import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
 import axios from 'axios';
 
 class App extends Component {
-  state = { };
+  state = {
+    name: '',
+    surname: '',
+    city: '',
+    address: '',
+    phone: '',
+    person: null
+  };
 
-  
+  componentDidMount() {
+    this.loadUsers();
+  }
 
-changeNameHandler = (e) => {
-  this.setState({ name: e.target.value})
-}
-changeSurnameHandler = (e) => {
-  this.setState({surname: e.target.value})
-}
-changeCityHandler = (e) => {
-  this.setState({city: e.target.value})
-}
-changeAddressHandler = (e) => {
-  this.setState({address: e.target.value})
-}
-changePhoneHandler = (e) => {
-  this.setState({phone: e.target.value})
-  
-}
+  loadUsers = () => {
+    axios.get('https://phonebookapi.herokuapp.com/api/allPersons')
+      .then(response => {
+        this.setState({ ...this.state, persons: response.data.persons });
+      });
+  }
 
-postUser = () => {
-  axios.post('https://phonebookapi.herokuapp.com/api/createPerson', {
-  name: this.state.name,
-  surname: this.state.surname,
-  city: this.state.city,
-  address: this.state.address,
-  phone: this.state.phone
-  })
-  .then(response =>{
-    if(response === 200) {
-      this.props.saljem
-    }
-  })
-  .catch(error =>{
-console.log(error);
-  });
-  
-}
 
-   league = () =>{
+
+
+  changeNameHandler = (e) => {
+    this.setState({ name: e.target.value })
+  }
+  changeSurnameHandler = (e) => {
+    this.setState({ surname: e.target.value })
+  }
+  changeCityHandler = (e) => {
+    this.setState({ city: e.target.value })
+  }
+  changeAddressHandler = (e) => {
+    this.setState({ address: e.target.value })
+  }
+  changePhoneHandler = (e) => {
+    this.setState({ phone: e.target.value })
+
+  }
+
+  postUser = () => {
+    axios.post('https://phonebookapi.herokuapp.com/api/createPerson', {
+      name: this.state.name,
+      surname: this.state.surname,
+      city: this.state.city,
+      address: this.state.address,
+      phone: this.state.phone
+    })
+      .then(response => {
+        console.log(response);
+        if (response.status === 200) {
+          this.loadUsers()
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+
+
+
+
+  league = () => {
     console.log("Ja sam parent funckija, ali me pozivas iz childa");
   }
 
   render() {
-    
+
     return (
       <div className="App">
         <div className="nebo container">
@@ -58,15 +81,15 @@ console.log(error);
             <div className="col-md-3">
               <div className="form-group inputWrapper">
                 <h5 className="textLabel">Please enter your data</h5>
-                <input type="text" className="form-control inputFilds"  name='name'    placeholder="Name"    onChange={this.changeNameHandler}/>
-                <input type="text" className="form-control inputFilds"  name='surname' placeholder="Surname" onChange={this.changeSurnameHandler} />
-                <input type="text" className="form-control inputFilds"  name='city'    placeholder="City"    onChange={this.changeCityHandler}/>
-                <input type="text" className="form-control inputFilds"  name='address' placeholder="Address" onChange={this.changeAddressHandler} />
-                <input type="text" className="form-control inputFilds"  name='phone'   placeholder="Phone"   onChange={this.changePhoneHandler}/>
-                <input type='submit' color='warning'  onClick={this.postUser} style={{marginTop: '4%', marginLeft: '18%'}} value="add user" />
-              </div>>
+                <input type="text" className="form-control inputFilds" name='name' placeholder="Name" onChange={this.changeNameHandler} />
+                <input type="text" className="form-control inputFilds" name='surname' placeholder="Surname" onChange={this.changeSurnameHandler} />
+                <input type="text" className="form-control inputFilds" name='city' placeholder="City" onChange={this.changeCityHandler} />
+                <input type="text" className="form-control inputFilds" name='address' placeholder="Address" onChange={this.changeAddressHandler} />
+                <input type="text" className="form-control inputFilds" name='phone' placeholder="Phone" onChange={this.changePhoneHandler} />
+                <input type='submit' color='warning' onClick={this.postUser} style={{ marginTop: '4%', marginLeft: '18%' }} value="add user" />
+              </div>
             </div>
-            <Persons saljem={this.loadUsers} />
+            <Persons saljem={this.state.persons} izbrisiOsobe={this.loadUsers} />
 
           </div>
         </div>
--- src/components/persons.js
+++ src/components/persons.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Button } from 'reactstrap';
+import SkyLight from 'react-skylight';
 
 
 class Person extends Component {
@@ -9,33 +10,72 @@ class Person extends Component {
         surname: '',
         city: '',
         address: '',
-        phone: '',
+        phone: ''
     }
 
 
-    componentDidMount() {
-     this.loadUsers();
-    }
+    // componentDidMount() {
 
-    loadUsers = () => {
-        axios.get('https://phonebookapi.herokuapp.com/api/allPersons')
-        .then(response => {
-            this.setState({ ...this.state, persons: response.data.persons });
-        });
-    }
+    // }
+
 
     deleteUser = (user) => {
         axios.delete('https://phonebookapi.herokuapp.com/api/deletePerson/' + user)
-        .then(response => {
-            if (response.status === 200) {
-                this.loadUsers()
-            }
-        });
+            .then(response => {
+                if (response.status === 200) {
+                    this.props.izbrisiOsobe();
+                }
+            });
     }
 
+    editUser = (person) => {
+        this.simpleDialog.show()
+        this.setState({ ...this.state, personID: person._id, name: person.name, surname: person.surname, city: person.city, address: person.address, phone: person.phone })
+
+    }
+
+    nameHandler = (e) => {
+        console.log("bilo sta");
+        this.setState({ name: e.target.value })
+    }
+    surnameHandler = (e) => {
+        this.setState({ surname: e.target.value })
+    }
+    cityHandler = (e) => {
+        this.setState({ city: e.target.value })
+    }
+    addressHandler = (e) => {
+        this.setState({ address: e.target.value })
+    }
+    phoneHandler = (e) => {
+        this.setState({ phone: e.target.value })
+    }
+
+    updateUser = () => {
+        axios.patch('https://phonebookapi.herokuapp.com/api/updatePerson', {
+            name: this.state.name,
+            surname: this.state.surname,
+            city: this.state.city,
+            address: this.state.address,
+            phone: this.state.phone,
+            personID: this.state.personID
+        })
+            .then(response => {
+                console.log(response)
+                if (response.status === 200) {
+                    this.props.izbrisiOsobe();
+                    this.simpleDialog.hide()
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+    }
+
+
     render() {
-        console.log(this.props);
-        if (!this.state.persons) {
+        if (!this.props.saljem) {
             return <h1>...Loading</h1>
         } else {
             return (
@@ -53,7 +93,7 @@ class Person extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                            {this.state.persons.map(person => {
+                            {this.props.saljem.map(person => {
 
                                 let godina = person.createdAt.slice(0, 4);
                                 let mesec = person.createdAt.slice(5, 7);
@@ -62,7 +102,7 @@ class Person extends Component {
 
 
                                 return (
-                                    <tr>
+                                    <tr key={person._id}>
                                         <td>{person.name}</td>
                                         <td>{person.surname}</td>
                                         <td>{fullDate}</td>
@@ -70,7 +110,8 @@ class Person extends Component {
                                         <td>{person.address}</td>
                                         <td>{person.phone}</td>
                                         <td>
-                                            <Button color='primary' onClick={this.props.saljem} style= {{marginRight: '4%'}} >Edit</Button>
+                                            <Button color='primary' onClick={this.editUser.bind(this, person)}
+                                                style={{ marginRight: '4%' }} >Edit</Button>
                                             <Button color='danger' onClick={this.deleteUser.bind(this, person._id)}>Delete</Button>
                                         </td>
                                     </tr>
@@ -79,6 +120,19 @@ class Person extends Component {
 
                         </tbody>
                     </table>
+                    <SkyLight hideOnOverlayClicked ref={ref => this.simpleDialog = ref} >
+
+                        <div className="form-group inputWrapper">
+                            <h5 className="textLabel">Please enter your data</h5>
+                            <input type="text" className="form-control inputFilds" value={this.state.name} onChange={this.nameHandler} name='name' placeholder="Name" />
+                            <input type="text" className="form-control inputFilds" value={this.state.surname} onChange={this.surnameHandler} name='surname' placeholder="Surname" />
+                            <input type="text" className="form-control inputFilds" value={this.state.city} name='city' onChange={this.cityHandler} placeholder="City" />
+                            <input type="text" className="form-control inputFilds" value={this.state.address} name='address' onChange={this.addressHandler} placeholder="Address" />
+                            <input type="text" className="form-control inputFilds" value={this.state.phone} name='phone' onChange={this.phoneHandler} placeholder="Phone" />
+                            <input type='submit' color='warning' onClick={this.updateUser} style={{ marginTop: '4%', marginLeft: '18%' }} value="add user" />
+                        </div>
+
+                    </SkyLight>
                 </div>
             )
         }
